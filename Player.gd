c231056a87cd8ced53b93d31effca860ab376490
Jordan Haus <jordanhaus69@gmail.com>
2020-11-26 12:43:28 -0500

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
-var motionSpeed = 150
+export var motionSpeed = 150
 var target = Vector2()
 var velocity = Vector2()
+var buffer = 5
+var animation_vector = Vector2.ZERO
+
+onready var animationtree = $AnimationTree
+onready var animationplayer = $AnimationPlayer
 
 func _input(event):
 	if event.is_action("left_click"):
@@ -11,5 +16,6 @@ func _input(event):
 func _physics_process(delta):
 	velocity = (target - position).normalized() * motionSpeed
 	# rotation = velocity.angle()
-	if (target - position).length() > 5:
+	if (target - position).length() > buffer:
 		velocity = move_and_slide(velocity)
+
---
+++ scripts/monster_obj.gd
@@ -0,0 +1,7 @@
+extends StaticBody2D
+
+var monster_check = false
+var 
+
+
+
